81b90d132208b852bdfb653cf1b4726cec91ad7b
David Nazariego Ulacio <114948954+nazariegode@users.noreply.github.com>
2023-07-11 23:37:28 -0400

    Update handOnLab.js

--- handOnLab.js
+++ handOnLab.js
@@ -1,1 +1,29 @@
+class ProductManager {
+    constructor (){
+        this.products = [] 
+    }
 
+    getProduct(){
+        return this.products
+    }
+
+    getProductById(id){
+        const busqueda = this.products.find(idProduct => idProduct.id == id)
+            console.log(busqueda)
+       
+    }
+
+    addProduct(id, title, description, price, thumbnail, code, stock){
+        if(!title||!description||!price||!thumbnail||!code||!stock){
+            console.log('todos los campos deben ser requeridos')
+            return 
+        }
+        if(this.products.find(valorProducto => valorProducto.code == code)){
+            console.log('Su producto esta repetido')
+        }
+        const newProduct = {
+            id, title, description, price, thumbnail, code, stock
+        }
+        this.products.push(newProduct)
+    }
+}
